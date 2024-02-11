41a5201139577a75edd8b8c8447b2bcd1a5d95d9
Jayant Ghadge <jayantghadge6@gmail.com>
2024-02-11 22:51:23 +0530

    removed proxy config for API fetching

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,32 +1,35 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-games',
   templateUrl: './games.component.html',
-  styleUrl: './games.component.scss'
+  styleUrls: ['./games.component.scss'],
 })
 export class GamesComponent {
   type = '';
   id = '';
-  url='';
+  url = '';
   games: any;
   game: any;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient ){}
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
-    this.url = `/api/game?id=${this.id}`;
+    this.url = `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${this.id}`;
     this.getGame();
-
   }
 
-  getGame(){
-    console.log(this.url)
-    this.http.get(this.url).subscribe((games)=>{
-      this.game = games;
-    })
+  getGame() {
+    const headers = new HttpHeaders().set(
+      'X-Rapidapi-Key',
+      'f9ea1431e2mshedd67e8b03ee1a0p10eaa4jsn1c893aa7579d'
+    );
+
+    this.http.get(this.url, { headers }).subscribe((game) => {
+      this.game = game;
+    });
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,52 +5,95 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   trendingGames: any;
   popularGames: any;
-  latestGames:any;
+  latestGames: any;
   ratedGames: any;
   sliderValue = 0;
-  constructor(private http: HttpClient, private router: Router){}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  ngOnInit(): void{
+  ngOnInit(): void {
     this.getTrendingGames();
     this.getPopularGames();
     this.getLatestGames();
     this.getRatedGames();
   }
   getTrendingGames() {
-    this.http.get('/api/games?sort-by=trending').subscribe((games) => {
-      this.trendingGames = games;
-      console.log(this.trendingGames);
-    });
+    this.http
+      .get(
+        'https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=trending',
+        {
+          headers: {
+            'X-Rapidapi-Key':
+              'f9ea1431e2mshedd67e8b03ee1a0p10eaa4jsn1c893aa7579d',
+            'X-Rapidapi-Host': 'free-to-play-games-database.p.rapidapi.com',
+          },
+        }
+      )
+      .subscribe((games) => {
+        this.trendingGames = games;
+        console.log(this.trendingGames);
+      });
   }
 
   getPopularGames() {
-    this.http.get('/api/games?sort-by=popularity').subscribe((games) => {
-      this.popularGames = games;
-      console.log(this.popularGames);
-    });
+    this.http
+      .get(
+        'https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=popularity',
+        {
+          headers: {
+            'X-Rapidapi-Key':
+              'f9ea1431e2mshedd67e8b03ee1a0p10eaa4jsn1c893aa7579d',
+            'X-Rapidapi-Host': 'free-to-play-games-database.p.rapidapi.com',
+          },
+        }
+      )
+      .subscribe((games) => {
+        this.popularGames = games;
+        console.log(this.popularGames);
+      });
   }
 
   getRatedGames() {
-    this.http.get('/api/games?sort-by=rating').subscribe((games) => {
-      this.ratedGames = games;
-      console.log(this.ratedGames);
-    });
+    this.http
+      .get(
+        'https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=rating',
+        {
+          headers: {
+            'X-Rapidapi-Key':
+              'f9ea1431e2mshedd67e8b03ee1a0p10eaa4jsn1c893aa7579d',
+            'X-Rapidapi-Host': 'free-to-play-games-database.p.rapidapi.com',
+          },
+        }
+      )
+      .subscribe((games) => {
+        this.ratedGames = games;
+        console.log(this.ratedGames);
+      });
   }
 
   getLatestGames() {
-    this.http.get('/api/games?sort-by=release-date').subscribe((games) => {
-      this.latestGames = Object.values(games).slice(0, 12);
-      console.log(this.latestGames);
-    });
+    this.http
+      .get(
+        'https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=release-date',
+        {
+          headers: {
+            'X-Rapidapi-Key':
+              'f9ea1431e2mshedd67e8b03ee1a0p10eaa4jsn1c893aa7579d',
+            'X-Rapidapi-Host': 'free-to-play-games-database.p.rapidapi.com',
+          },
+        }
+      )
+      .subscribe((games) => {
+        this.latestGames = Object.values(games).slice(0, 12);
+        console.log(this.latestGames);
+      });
   }
-  
-  goToGame(type:string, id:string){
-    this.router.navigate(['game', type,id])
+
+  goToGame(type: string, id: string) {
+    this.router.navigate(['game', type, id]);
   }
-  
-}  
+}
